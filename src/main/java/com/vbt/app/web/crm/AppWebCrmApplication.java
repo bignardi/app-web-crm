3b4c31fe9f7164d64dc8b719e20d6891a55eb1e5
bignardi <vinicius.bignardi@outlook.com>
2021-03-15 22:58:57 -0300

    Uploading application to GitHub

---
+++ src/main/java/com/vbt/app/web/crm/AppWebCrmApplication.java
@@ -0,0 +1,13 @@
+package com.vbt.app.web.crm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppWebCrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppWebCrmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vbt/app/web/crm/AppWebCrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vbt.app.web.crm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppWebCrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
